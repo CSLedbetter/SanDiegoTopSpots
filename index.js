eb3968a16b8bf0f10a6efa1c7989e97aa64414bb
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-06-13 16:44:46 -0700

    Linked JSON to project

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $.getJSON('https://raw.githubusercontent.com/OriginCodeAcademy/Cohort12/master/Practical/Week%202/02-SanDiegoTopSpots/topspots.json', function (spots) {
+        $.each(spots, function (i, data) {
+            var name = data.name;
+            var desc = data.description;
+            var link = data.location
+            console.log(link);
+            $('table').append('<tr><td>' + name + '</td>')
+            $('table tr:last').append('<td>' + desc + '</td>')
+            $('table tr:last').append('<td>' + '<a href=' + 'https://www.google.com/maps?q=' + link + '>' + 'Open in Google Maps' + "</a>" + '</td> </tr>');
+        });
+    });
+});

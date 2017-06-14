ccda247fa79dae0b7f9d1112292ab7775d4487e8
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-06-14 09:51:09 -0700

    Refactored, (added head and body tags to table, missing semicolons)

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
         $.each(spots, function (i, data) {
             var name = data.name;
             var desc = data.description;
-            var link = data.location
+            var link = data.location;
             console.log(link);
-            $('table').append('<tr><td>' + name + '</td>')
-            $('table tr:last').append('<td>' + desc + '</td>')
+            $('table').append('<tr><td>' + name + '</td>');
+            $('table tr:last').append('<td>' + desc + '</td>');
             $('table tr:last').append('<td>' +'<button type="button" class="btn btn-primary"'+ '<a href=' + 'https://www.google.com/maps?q=' + link + '>' + 'Open in Google Maps' + "</a>" + '</td> </tr>');
         });
     });

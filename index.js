89338871371ee34a9cc6d401e0457b9b6889fb3f
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-06-14 11:52:08 -0700

    Refactored further, took out button and added class to the <a> tag

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ $(document).ready(function () {
             var name = data.name;
             var desc = data.description;
             var link = data.location;
-            console.log(link);
             $('table').append('<tr><td>' + name + '</td>');
             $('table tr:last').append('<td>' + desc + '</td>');
-            $('table tr:last').append('<td>' +'<button type="button" class="btn btn-primary"'+ '<a href=' + 'https://www.google.com/maps?q=' + link + '>' + 'Open in Google Maps' + "</a>" + '</td> </tr>');
+            $('table tr:last').append('<td>' + '<a class="btn btn-primary" href=' + 'https://www.google.com/maps?q=' + link + '>' + 'Open in Google Maps' + "</a>" + '</td> </tr>');
         });
     });
 });

448a96c1d8746fd8e3a0a349baa832c71e33fb12
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-06-13 17:08:57 -0700

    Added some bootstrap visuals. Done at this point.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             console.log(link);
             $('table').append('<tr><td>' + name + '</td>')
             $('table tr:last').append('<td>' + desc + '</td>')
-            $('table tr:last').append('<td>' + '<a href=' + 'https://www.google.com/maps?q=' + link + '>' + 'Open in Google Maps' + "</a>" + '</td> </tr>');
+            $('table tr:last').append('<td>' +'<button type="button" class="btn btn-primary"'+ '<a href=' + 'https://www.google.com/maps?q=' + link + '>' + 'Open in Google Maps' + "</a>" + '</td> </tr>');
         });
     });
 });
